8fa00867ff82c363a1d2403084a70c559e7e62a4
Brant-Harker <brant.harker12@gmail.com>
2022-11-17 13:21:55 -0700

    Functioning getName with error checking

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ def main():
     #test = Event_class.Event_Schedule('Meeting', 2022, 11, 17, 17, 0, 'Discussing the project')
     #print(f"Today you have a {test.event_name}")
 
-    choice = -1
+    choice = ''
 
-    while choice != 5:
+    while choice != '5':
         choice = menu.printOptions()
+
+        if choice == '1': menu.createEvent()
 
 if __name__ == '__main__':
     main()
--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ TITLE = "menu.py 1.00 2022/11/17"
 This module contains functions for the
 menu in the terminal.
 """
+import Event_class
 
 # Prints the default menu, and returns the user's
 # selected option in the form of an integer
@@ -13,4 +14,39 @@ def printOptions():
                     "4. See your events\n" \
                     "5. Exit the program\n" \
                     "Please select an option by entering the number: ")
-    return int(choice)
+    return choice
+
+# Asks for the name of an event from the user,
+# and returns it. Error checks for entering blank spaces,
+# quits when entering nothing
+def getName():
+    name = input("Please enter the name of the event: ")
+    while name:
+
+        # If name contains non-space characters, return name
+        for c in name:
+            if c != ' ': return name
+        
+        name = input("\nThe event name cannot be blank\n" \
+                     "If you would like to cancel, press enter\n" \
+                     "Please enter a valid event name: ")
+    
+    return name
+
+# Creates an event object and stores it in a file.
+# Calls getName, 
+def createEvent():
+    name = getName()
+
+    if not name: 
+        print("\nEvent creation cancelled")
+        return False
+
+    date = input("Please enter the date of the event: ")
+
+    time = input("Please enter the time the event starts: ")
+
+    details = input("Please enter any notes about the event you'd like to remember: ")
+
+if __name__ == '__main__':
+    createEvent()
