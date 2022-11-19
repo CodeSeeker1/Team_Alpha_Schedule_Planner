e43f34961a8be8dafb78df95240f28537502cf66
Brant-Harker <brant.harker12@gmail.com>
2022-11-19 12:00:17 -0700

    Created Event_list.py

---
+++ Event_list.py
@@ -0,0 +1,15 @@
+TITLE = "Event_list.py 1.00 2022-11-19"
+"""-----------------------------------------------
+Functions to add, delete, and sort
+a list of Event_Schedule objects.
+
+Also includes reading data from a file
+to the list, and writing data from the list to
+a file.
+-----------------------------------------------"""
+import Event_class
+
+if __name__ == '__main__':
+    print(TITLE)
+# Testing
+    l = []
